298756256da0feeeba520426f282030f0cedf2d1
Ivana Andric <ivevil@gmail.com>
2023-01-09 21:24:58 +0100

    Create UI Table component

---
+++ src/components/UI/Table/index.tsx
@@ -0,0 +1,26 @@
+import './table.css'
+
+interface TableProps {
+    children: React.ReactNode;
+    firstRowArray: Array<string>;
+}
+
+const Table: React.FC<TableProps> = ({ children, firstRowArray }) => {
+
+    return (
+        <>
+            <table>
+                <tbody>
+                    <tr>
+                        {
+                            firstRowArray.map((firstRowItem, index) => <th key={index}>{firstRowItem}</th>
+                            )}
+                    </tr>
+                    {children}
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default Table;
